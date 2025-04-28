afaa1aee12bb994aef0cdaaf16e2d20f10b5b0cf
Genivaldo <genivaldo.serra@live.com>
2025-04-27 22:28:56 -0300

    feat: Adicionando main

---
+++ main.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "this" {
+  name        = var.sg_name
+  description = "Security Group que permite acesso SSH e TCP na porta 3000"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "Permitir acesso SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Permitir acesso TCP na porta 3000"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Permitir saida para qualquer destino"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.sg_name
+  }
+}
